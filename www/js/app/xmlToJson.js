c38eb5a6170cb28f3b66565e5c66d5d8465e3e28
Kirk Elliott <kirk@dmvjs.com>
2014-04-17 19:15:57 -0400

    lint friendly using item instead of story for feedObj property

--- www/js/app/xmlToJson.js
+++ www/js/app/xmlToJson.js
@@ -1,22 +1,20 @@
+/*global module, require*/
 module.exports = function (res) {
 	var feedObject = {}
     , root = res.firstChild.firstChild
     , numberOfFeeds = root.childNodes.length - 2
-    , nodesInFeed = root.childNodes[2].childNodes.length
     , i
     , j;
 
   feedObject.title = root.childNodes[0].textContent;
   feedObject.lastBuildDate = root.childNodes[1].textContent;
-  feedObject.story = [];
+  feedObject.item = [];
 
   for (i = 0; i < numberOfFeeds; i += 1) {
-      feedObject.story[i] = {};
-      for (j = 0; j < root.childNodes[2 + i].childNodes.length; j += 1) {
-          feedObject.story[i][root.childNodes[2 + i].childNodes[j].nodeName] =
-              root.childNodes[2 + i].childNodes[j].textContent;
-      }
+    feedObject.item[i] = {};
+    for (j = 0; j < root.childNodes[2 + i].childNodes.length; j += 1) {
+      feedObject.item[i][root.childNodes[2 + i].childNodes[j].nodeName] = root.childNodes[2 + i].childNodes[j].textContent;
+    }
   }
-
   return feedObject;
-}
+};
