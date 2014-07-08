fcda396e922ef005a1d50166fd876853c771cd49
Kirk Elliott <kirk@dmvjs.com>
2014-07-08 19:03:11 -0400

    fixed white space, added comments, slight refactor

--- www/js/io/readFile.js
+++ www/js/io/readFile.js
@@ -1,33 +1,28 @@
+/*global module, require*/
 var removeFile = require('./removeFile');
 
 module.exports = function (fileentry) {
-    var reader = new FileReader()
-	    , errorHandler = window.onerror
-	    , restoreHandler = function () {
-		    window.onerror = errorHandler;
-	    };
+	var reader = new FileReader()
+		, errorHandler = window.onerror
+		, restoreHandler = function () {
+				window.onerror = errorHandler;
+			};
 
-    return new Promise(function (resolve, reject) {
-	      window.onerror = function (err) {
-		      console.log(err, arguments);
-		      removeFile(fileentry).then(function () {
-			      restoreHandler();
-			      reject()
-		      }, function () {
-			      restoreHandler();
-			      reject()
-		      })
-	      };
-        fileentry.file(function (f) {
-          reader.onloadend = function (s) {
-	          restoreHandler();
-	          resolve(s);
-          };
-          reader.onerror = function (e) {
-	          restoreHandler()
-	          reject(e);
-          };
-          reader.readAsText(f);
-        })
-    });
+	return new Promise(function (resolve, reject) {
+		var rejection = function (err) {
+			restoreHandler();
+			reject(err);
+		};
+		window.onerror = function (err) {
+			removeFile(fileentry).then(rejection, rejection)
+		};
+		fileentry.file(function (f) {
+			reader.onloadend = function (s) {
+				restoreHandler();
+				resolve(s);
+			};
+			reader.onerror = rejection;
+			reader.readAsText(f);
+		})
+	});
 };
