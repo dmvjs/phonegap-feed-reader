2af48662c5026aa47d9e87709a6aa2fecb18885c
Kirk Elliott <kirk@dmvjs.com>
2014-07-08 19:02:45 -0400

    fixed white space, added comments, slight refactor

--- www/js/io/createFIleWithContents.js
+++ www/js/io/createFIleWithContents.js
@@ -6,15 +6,21 @@ var getFileSystem = require('./getFileSystem')
 
 module.exports = function (filename, contents) {
 	return new Promise(function (resolve, reject) {
+
+		// full set of Crockford's problem characters https://github.com/douglascrockford/JSON-js/blob/master/json2.js
+		//[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff
+		var reg = /[\u000a\u000d\u2028\u2029]/g; // known problem characters, line terminators
+		contents = contents.replace(reg, '');
+
 		try {
-			JSON.parse(contents)
-			var reg = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff']/g;
-			contents = contents.replace(reg, '');
-		}
-		catch (e) {
-			analytics.trackEvent('Feed', 'Error', 'JSON Parse Error: ' + filename, 10);
+			JSON.parse(contents);
+		} catch (e) {
+			if (void 0 !== window.analytics) {
+				analytics.trackEvent('Feed', 'Error', 'JSON Parse Error: ' + filename, 10);
+			}
 			reject()
 		}
+
 		getFileSystem().then(function (filesystem) {
 			getFile(filesystem, filename, true).then(function (fileentry) {  
 				getFileEntry(fileentry).then(function (filewriter) {
