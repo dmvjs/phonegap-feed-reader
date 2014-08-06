4001121641e01dffb6b15808572a13738bc97f39
Kirk Elliott <kirk@dmvjs.com>
2014-08-06 11:48:16 -0400

    added gulpfile

---
+++ www/js/gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp')
+	, shell = require('gulp-shell');
+
+gulp.task('default', shell.task([
+	'browserify index.js -o app.js',
+	'uglifyjs app.js -o app.min.js',
+	'cordova build ios'
+]));
